712605967fb4e1774d70eff365da25509f883f00
maximdushniak <maxim.dushniak@gmail.com>
2015-09-17 11:36:01 +0300

    ...

--- HelloWorld/src/YetAnotherHelloWorld.java
+++ HelloWorld/src/YetAnotherHelloWorld.java
@@ -1,5 +1,16 @@
+/**
+ * Prints <code>Hello world</>
+ * @author Max
+ * @version 1.0
+ */
 public class YetAnotherHelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        //single line comment
+
+        /*
+        multi-line comment
+         */
+
     }
 }
